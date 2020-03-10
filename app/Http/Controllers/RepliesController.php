b2dc08b3f28c21b12e8322521876faa237295d2c
Anisur Rahman <anisurlist3@gmail.com>
2020-03-10 11:35:23 +0600

    lesson 32 | edit reply complete

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -41,5 +41,13 @@ class RepliesController extends Controller
 
         return back(); 
     }
+
+    public function update(Reply $reply)
+    {
+        $this->authorize('update', $reply);
+        $reply->update(request(['body']));
+
+        // return back(); 
+    }
     
 }
